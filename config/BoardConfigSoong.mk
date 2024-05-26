bb5b1628ada0ddaa61ee11d61a32575a430104af
Dhina17 <dhinalogu@gmail.com>
2024-05-26 15:27:50 +0530

    soong: Add libcameraservice extension config
    Change-Id: Id20f782c1008fed1f15250129618326091f61bf8
    
    hridaya2004: Move libcameraservice extension lib to select()

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -36,6 +36,11 @@ ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
     $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
 endif
 
+# libcameraservice extension
+ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
+    $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
+endif
+
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
     $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
