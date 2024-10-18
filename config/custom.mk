1e0677c4ab5ffe450759aa1885703d2d41cd0151
Alberto Ponces <ponces26@gmail.com>
2024-10-18 19:11:46 +0000

    config: Switch to AOSPA's face unlock implementation
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/custom.mk
+++ config/custom.mk
@@ -18,6 +18,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Fonts
 include vendor/aosp/fonts/fonts.mk
 
