dca57dfa65f16a4eed0e7f33ee1471679c7aca13
someone5678 <someone5678@users.noreply.github.com>
2025-02-04 04:43:50 +0000

    vendor: Import PixelLauncher Overlays

--- config/custom.mk
+++ config/custom.mk
@@ -57,5 +57,9 @@ $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 # Don't dexpreopt prebuilts. (For GMS).
 DONT_DEXPREOPT_PREBUILTS := true
 
+# PixelLauncherOverlays
+PRODUCT_PACKAGES += \
+    PixelLauncherOverlayBlur
+
 # Themed icons for Pixel Launcher
 $(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
